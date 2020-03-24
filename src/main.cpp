129afe258c0bd72400ae0d38cf2b09e3bf00f50e
Rémi DESCHAMPS <remi.deschamps@epitech.eu>
2020-03-24 17:20:22 +0100

    added main

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2020
+** Aracde_gitisdown
+** File description:
+** main
+*/
+
+#include "core.hpp"
+#include "ecs.hpp"
+#include "event.hpp"
+#include "game.hpp"
+#include "graphical.hpp"
+
+int main(void)
+{
+    return (0);
+}
